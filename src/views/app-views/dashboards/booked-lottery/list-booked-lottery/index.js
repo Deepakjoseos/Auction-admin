bd3887889dd0592b4a9436fa1e4a75b54e731f3e
iamvishalchandra <vishal.chandra75@gmail.com>
2022-07-28 13:32:09 +0530

    Push: 6, Branch: booked-lottery-date, Update: Converted timestamp to time

--- src/views/app-views/dashboards/booked-lottery/list-booked-lottery/index.js
+++ src/views/app-views/dashboards/booked-lottery/list-booked-lottery/index.js
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { Card, Table, Input, Menu } from 'antd'
+import React, { useEffect, useState } from "react";
+import { Card, Table, Input, Menu } from "antd";
 // import BrandListData from 'assets/data/product-list.data.json'
-import { EyeOutlined, SearchOutlined } from '@ant-design/icons'
-import EllipsisDropdown from 'components/shared-components/EllipsisDropdown'
-import Flex from 'components/shared-components/Flex'
-import { useHistory } from 'react-router-dom'
-import utils from 'utils'
-import bookedLotteryService from 'services/BookedLottery'
+import { EyeOutlined, SearchOutlined } from "@ant-design/icons";
+import EllipsisDropdown from "components/shared-components/EllipsisDropdown";
+import Flex from "components/shared-components/Flex";
+import { useHistory } from "react-router-dom";
+import utils from "utils";
+import bookedLotteryService from "services/bookedLottery";
 
 const BookedLotteryList = () => {
-  let history = useHistory()
+  let history = useHistory();
 
-  const [list, setList] = useState([])
-  const [searchBackupList, setSearchBackupList] = useState([])
+  const [list, setList] = useState([]);
+  const [searchBackupList, setSearchBackupList] = useState([]);
 
   useEffect(() => {
     // Getting Lotteries List to display in the table
     const getLottoryTypes = async () => {
-      const data = await bookedLotteryService.getBookedLotteries()
+      const data = await bookedLotteryService.getBookedLotteries();
       if (data) {
-        setList(data)
-        setSearchBackupList(data)
+        setList(data);
+        setSearchBackupList(data);
       }
-    }
-    getLottoryTypes()
-  }, [])
+    };
+    getLottoryTypes();
+  }, []);
 
   // Dropdown menu for each row
   const dropdownMenu = (row) => (
@@ -36,17 +36,17 @@ const BookedLotteryList = () => {
         </Flex>
       </Menu.Item>
     </Menu>
-  )
+  );
 
   const viewDetails = (row) => {
-    history.push(`/app/dashboards/booking/${row.id}`)
-  }
+    history.push(`/app/dashboards/booking/${row.id}`);
+  };
 
   // Antd Table Columns
   const tableColumns = [
     {
-      title: 'Booking',
-      dataIndex: 'booking',
+      title: "Booking",
+      dataIndex: "booking",
       render: (booking) => (
         <Flex alignItems="center">{booking?.bookingNumber}</Flex>
       ),
@@ -54,50 +54,50 @@ const BookedLotteryList = () => {
         a.booking.bookingNumber.localeCompare(b.booking.bookingNumber),
     },
     {
-      title: 'Number',
-      dataIndex: 'lotteryNumber',
-      sorter: (a, b) => utils.antdTableSorter(a, b, 'bookingNumber'),
+      title: "Number",
+      dataIndex: "lotteryNumber",
+      sorter: (a, b) => utils.antdTableSorter(a, b, "bookingNumber"),
     },
     {
-      title: 'Count',
-      dataIndex: 'count',
+      title: "Count",
+      dataIndex: "count",
       sorter: (a, b) => a.count - b.count,
     },
     {
-      title: 'Date',
-      dataIndex: 'drawDate',
-      sorter: (a, b) => utils.antdTableSorter(a, b, 'drawDate'),
+      title: "Date",
+      dataIndex: "drawDate",
+      sorter: (a, b) => utils.antdTableSorter(a, b, "drawDate"),
     },
     {
-      title: 'Paid Amount',
-      dataIndex: 'paidAmount',
+      title: "Paid Amount",
+      dataIndex: "paidAmount",
       sorter: (a, b) => a.paidAmount - b.paidAmount,
     },
     {
-      title: 'Agent',
-      dataIndex: 'booking',
+      title: "Agent",
+      dataIndex: "booking",
       render: (booking) => (
         <Flex alignItems="center">{booking?.agent?.name}</Flex>
       ),
       sorter: (a, b) => a.agent.name.localeCompare(b.booking.agent.name),
     },
     {
-      title: 'Lottery',
-      dataIndex: 'lottery',
+      title: "Lottery",
+      dataIndex: "lottery",
       render: (lottery) => <Flex alignItems="center">{lottery?.name}</Flex>,
       sorter: (a, b) => a.lottery.name.localeCompare(b.lottery.name),
     },
     {
-      title: 'Group',
-      dataIndex: 'lotteryGroup',
+      title: "Group",
+      dataIndex: "lotteryGroup",
       render: (lotteryGroup) => (
         <Flex alignItems="center">{lotteryGroup?.group}</Flex>
       ),
       sorter: (a, b) => a.lotteryGroup?.group - b.lotteryGroup?.group,
     },
     {
-      title: 'Type',
-      dataIndex: 'lotteryType',
+      title: "Type",
+      dataIndex: "lotteryType",
       render: (lotteryType) => (
         <Flex alignItems="center">{lotteryType?.name}</Flex>
       ),
@@ -105,23 +105,23 @@ const BookedLotteryList = () => {
     },
 
     {
-      title: '',
-      dataIndex: 'actions',
+      title: "",
+      dataIndex: "actions",
       render: (_, elm) => (
         <div className="text-right">
           <EllipsisDropdown menu={dropdownMenu(elm)} />
         </div>
       ),
     },
-  ]
+  ];
 
   // When Search is used
   const onSearch = (e) => {
-    const value = e.currentTarget.value
-    const searchArray = e.currentTarget.value ? list : searchBackupList
-    const data = utils.wildCardSearch(searchArray, value)
-    setList(data)
-  }
+    const value = e.currentTarget.value;
+    const searchArray = e.currentTarget.value ? list : searchBackupList;
+    const data = utils.wildCardSearch(searchArray, value);
+    setList(data);
+  };
 
   // Table Filters JSX Elements
   const filters = () => (
@@ -134,7 +134,7 @@ const BookedLotteryList = () => {
         />
       </div>
     </Flex>
-  )
+  );
 
   return (
     <Card>
@@ -145,7 +145,7 @@ const BookedLotteryList = () => {
         <Table columns={tableColumns} dataSource={list} rowKey="id" />
       </div>
     </Card>
-  )
-}
+  );
+};
 
-export default BookedLotteryList
+export default BookedLotteryList;
--- src/views/app-views/dashboards/booking/index.js
+++ src/views/app-views/dashboards/booking/index.js
@@ -1,10 +1,10 @@
-import { Route, Switch, Redirect } from 'react-router-dom'
-import BookingList from './list-booking'
-import BookedList from './booked-lotteries'
+import { Route, Switch, Redirect } from "react-router-dom";
+import BookingList from "./list-booking";
+import BookedList from "./booked-lotteries";
 
 // In here we will define all our routes
 const Booking = (props) => {
-  const { match } = props
+  const { match } = props;
   return (
     <Switch>
       <Redirect exact from={`${match.url}`} to={`${match.url}/list`} />
@@ -14,7 +14,7 @@ const Booking = (props) => {
         component={BookedList}
       />
     </Switch>
-  )
-}
+  );
+};
 
-export default Booking
+export default Booking;
